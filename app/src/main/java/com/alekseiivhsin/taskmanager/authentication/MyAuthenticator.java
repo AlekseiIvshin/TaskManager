e907a5f1ed4c68ca9478bfb77bedd0a7ed7c97fa
Aleksei Ivshin <Aleksei_Ivshin@epam.com>
2015-11-25 19:32:29 +0400

    Add stubs

--- app/src/main/java/com/alekseiivhsin/taskmanager/authentication/MyAuthenticator.java
+++ app/src/main/java/com/alekseiivhsin/taskmanager/authentication/MyAuthenticator.java
@@ -5,7 +5,6 @@ import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
-import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -25,7 +24,7 @@ public class MyAuthenticator extends AbstractAccountAuthenticator {
 
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -36,7 +35,7 @@ public class MyAuthenticator extends AbstractAccountAuthenticator {
         intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
 
         final Bundle bundle = new Bundle();
-        if(options != null){
+        if (options != null) {
             bundle.putAll(options);
         }
 
@@ -46,46 +45,46 @@ public class MyAuthenticator extends AbstractAccountAuthenticator {
 
     @Override
     public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
         final Bundle result = new Bundle();
         final AccountManager accountManager = AccountManager.get(mContext.getApplicationContext());
-        String authToken = accountManager.peekAuthToken(account,authTokenType);
-        if(TextUtils.isEmpty(authToken)){
+        String authToken = accountManager.peekAuthToken(account, authTokenType);
+        if (TextUtils.isEmpty(authToken)) {
             final String password = accountManager.getPassword(account);
-            if(!TextUtils.isEmpty(password)){
-                authToken = AuthTokenLoader.signIn(mContext,account.name,password);
+            if (!TextUtils.isEmpty(password)) {
+                authToken = AuthTokenLoader.signIn(mContext, account.name, password);
             }
         }
 
-        if(!TextUtils.isEmpty(authToken)){
-            result.putString(AccountManager.KEY_ACCOUNT_NAME,account.name);
-            result.putString(AccountManager.KEY_ACCOUNT_TYPE,account.type);
-            result.putString(AccountManager.KEY_AUTHTOKEN,authToken);
+        if (!TextUtils.isEmpty(authToken)) {
+            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
         } else {
-            final Intent intent = new Intent(mContext,LoginActivity.class);
-            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE,response);
-            intent.putExtra(LoginActivity.EXTRA_TOKEN_TYPE,authTokenType);
-            result.putParcelable(AccountManager.KEY_INTENT,intent);
+            final Intent intent = new Intent(mContext, LoginActivity.class);
+            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+            intent.putExtra(LoginActivity.EXTRA_TOKEN_TYPE, authTokenType);
+            result.putParcelable(AccountManager.KEY_INTENT, intent);
         }
         return result;
     }
 
     @Override
     public String getAuthTokenLabel(String authTokenType) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 }
